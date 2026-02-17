36cc12fd8a8fa45ea1d99ff33594a82502735ed1
Mamta-gangwar <mamtagangwar0528@gmail.com>
2026-02-17 11:08:17 +0530

    Fix GitHub Pages blank page

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/Taskmanager/",
+  base: "/Task_manager/",
   plugins: [
     react(),
     tailwindcss(),
